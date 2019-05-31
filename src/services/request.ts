606afd8972274f56e420a7f80ee12bec4c4ff090
domen <mixiaofeng1996@gmail.com>
2019-05-31 22:09:41 +0800

    feat: 登录成功

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 interface Options extends AxiosRequestConfig {
-  /**替换的主机域名 */
-  host?: string;
+	/**替换的主机域名 */
+	host?: string;
 }
 
 const host = 'http://test.api.supplier.tdianyi.com/';
@@ -14,12 +14,12 @@ const host = 'http://test.api.supplier.tdianyi.com/';
  * 必要参数参考axios
  */
 export default function request(options: Options) {
-  /**验证token */
-  const token = localStorage.getItem('token');
-  /**合并headers */
-  options.headers = { ...options.headers, Authorization: token };
-  /**拼接接口地址 */
-  options.url = options.host ? options.host + options.url : host + options.url;
-  /**axios 请求 */
-  return axios(options);
+	/**验证token */
+	const token = localStorage.getItem('token');
+	/**合并headers */
+	options.headers = { ...options.headers, Authorization: token };
+	/**拼接接口地址 */
+	options.url = options.host ? options.host + options.url : host + options.url;
+	/**axios 请求 */
+	return axios(options).then(res => res.data);
 }
