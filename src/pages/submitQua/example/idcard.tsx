0b8e1b3ec7ab9f91fac684f0bc296b52089a0228
songyizhe <769724661@qq.com>
2019-05-31 17:59:46 +0800

    提交资质，创建门店，店铺信息的静态及一些功能

--- src/pages/submitQua/example/idcard.tsx
+++ src/pages/submitQua/example/idcard.tsx
@@ -13,7 +13,7 @@ export default class IdCardExample extends Component {
       <div style={{ width: '100%', height: '100%', background: '#fff' }}>
         <WingBlank>
            <Flex className={styles.title}>拍照示例：身份证正面照</Flex>
-           {/* <Flex className={styles.bg}></Flex> */}
+           <Flex className={styles.bg}>123</Flex>
            <Flex className={styles.footer}>请保证身份证各项信息清晰可见，无遮挡</Flex>
            <Button type="primary" style={{ marginTop: 60 }} className={styles.button} onClick={this.goBack}>
             已了解，去上传
