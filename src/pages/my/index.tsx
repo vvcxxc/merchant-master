8159d10faa24d03eeaebd3e0ec3cb69a96159c3b
domen <mixiaofeng1996@gmail.com>
2019-06-10 15:50:57 +0800

    feat: 对接

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,22 +1,69 @@
 /**title: 个人中心 */
 import React, { Component } from 'react';
-import { Flex, WingBlank } from 'antd-mobile';
+import { Flex, WingBlank, Toast } from 'antd-mobile';
 import styles from './index.less';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
+import request from '@/services/request';
+
+interface State {
+	info: Info;
+}
+
+interface Info {
+	id: number;
+	name: string;
+	preview: string;
+	wx_sign_status: number;
+	money: string;
+	wx_sign_url: string;
+	integral: number;
+	canInvite: number;
+	bank_count: number;
+}
 
 export default connect()(
-	class MyPage extends Component<any> {
+	class MyPage extends Component<any, State> {
+		state = {
+			info: {
+				id: 0,
+				name: '',
+				preview: '',
+				wx_sign_status: 0,
+				money: '',
+				wx_sign_url: '0',
+				integral: 0,
+				canInvite: 0,
+				bank_count: 1
+			}
+		};
+
 		/**跳转到页面 */
 		pushPage = (pathname: string) => () => this.props.dispatch(routerRedux.push({ pathname }));
+
+		componentDidMount() {
+			this.getMyInfo();
+		}
+
+		getMyInfo = async () => {
+			Toast.loading('');
+			const res = await request({
+				url: 'api/merchant/supplier/info'
+			});
+			Toast.hide();
+			if (res.code === 200) {
+				this.setState({ info: res.data });
+			}
+		};
+
 		render() {
 			return (
 				<div className={styles.page}>
 					<div className={styles.headInfo}>
 						<WingBlank>
 							<Flex className={styles.headInfoContent}>
-								<img src="" alt="" className="userImg" />
-								<Flex.Item className="name">店铺的名称</Flex.Item>
+								<img src={this.state.info.preview} alt="" className="userImg" />
+								<Flex.Item className="name">{this.state.info.name}</Flex.Item>
 								<img src={require('./setting.png')} alt="" className="setting" />
 							</Flex>
 						</WingBlank>
@@ -26,20 +73,20 @@ export default connect()(
 							<Flex align="start">
 								<Flex.Item className="benefit">
 									<div className="label">平台收益</div>
-									<div className="money">￥23,280.00 </div>
+									<div className="money">￥{this.state.info.money} </div>
 								</Flex.Item>
-								<div className="btn">提额</div>
+								<div className="btn">提现</div>
 							</Flex>
 							<Flex className="bottom">
 								<Flex.Item>
 									<Flex direction="column" className="card" onClick={this.pushPage('/my/bank')}>
 										<div>银行卡</div>
-										<div>1</div>
+										<div>{this.state.info.bank_count}</div>
 									</Flex>
 								</Flex.Item>
 								<div className="give" onClick={this.pushPage('/my/give')}>
 									<div className="label">礼品币</div>
-									<div className="value">23,280.00 </div>
+									<div className="value">{this.state.info.integral} </div>
 								</div>
 							</Flex>
 						</div>
