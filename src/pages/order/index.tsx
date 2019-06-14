faa85deb43670a67323f4a63e4686da53980e560
domen <mixiaofeng1996@gmail.com>
2019-06-14 14:08:56 +0800

    feat: 订单

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -7,33 +7,63 @@ import styles from './index.less';
 import { WingBlank, Flex, Toast } from 'antd-mobile';
 import FiltrateLayout from '../../components/layout';
 import request from '@/services/request';
+import moment from 'moment';
 
 export default class OrderPage extends Component {
 	state = {
-		list: [1, 2, 3, 4]
+		list: [1, 2, 3, 4],
+		insignificant: 0
 	};
 
-	getData = async () => {
+	undetermined = [
+		{
+			id: 0,
+			label: '所有订单'
+		},
+		{ id: 1, label: '已支付' },
+		{ id: 2, label: '已使用' },
+		{ id: 3, label: '已退款' },
+		{ id: 4, label: '已过期' }
+	];
+
+	componentDidMount() {
+		this.getData();
+	}
+
+	getData = async (query?: any) => {
 		Toast.loading('');
-		const res = await request({ url: '' });
+		const res = await request({ url: 'v3/coupons/order_list', ...query });
 		Toast.hide();
 		if (res.code === 200) {
-			this.setState({ list: res.data });
+			this.setState({ list: res.data, insignificant: res.total });
 		}
 	};
+
+	handleLayoutChange = (query: any) => {
+		this.getData({
+			pay_status: query.hot,
+			date: query.time
+		});
+	};
+
 	render() {
-		const orderList = this.state.list.map(_ => (
-			<Flex key={_} className={styles.orderItem}>
+		const orderList = this.state.list.map((_: any) => (
+			<Flex key={_.id} className={styles.orderItem}>
 				<img src="" alt="" />
 				<Flex.Item className="content">
-					<div className="ordernum">AB12345678901234</div>
-					<div className="time">2018-12-06 18:00</div>
+					<div className="ordernum">{_.youhui_sn}</div>
+					<div className="time">{moment.unix(_.create_time).format('YYYY-MM-DD hh:mm')}</div>
 				</Flex.Item>
-				<div className="status">已支付</div>
+				<div className="status">{_.status_msg}</div>
 			</Flex>
 		));
 		return (
-			<FiltrateLayout undetermined={[]} hasInsignificant={true}>
+			<FiltrateLayout
+				undetermined={this.undetermined}
+				hasInsignificant={true}
+				insignificant={`${this.state.insignificant}笔交易`}
+				onChange={this.handleLayoutChange}
+			>
 				{orderList}
 			</FiltrateLayout>
 		);
