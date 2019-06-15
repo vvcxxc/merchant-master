84a02e3109458c9266eb842aaf021a7211f44441
songyizhe <769724661@qq.com>
2019-06-15 18:18:20 +0800

    ios获取sdk测试4

--- src/pages/createStore/map/index.tsx
+++ src/pages/createStore/map/index.tsx
@@ -64,19 +64,20 @@ export default class MapPage extends Component {
           "openLocation"
         ]
       });
-      wx.getLocation({
-        type: 'wgs84',
-        success: function (res: any) {
-          let latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-          let longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-          let location = {
-            latitude,
-            longitude
-          };
-          _this.setState({location})
-
-        }
-      });
+      wx.ready(() => {
+        wx.getLocation({
+          type: 'wgs84',
+          success: function (res: any) {
+            let latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            let longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            let location = {
+              latitude,
+              longitude
+            };
+            _this.setState({location})
+          }
+        });
+      })
     });
   }
 
