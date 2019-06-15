d47feba47f5f0a11d620ee002dcc4f68ad037c6e
domen <mixiaofeng1996@gmail.com>
2019-06-15 17:21:51 +0800

    feat: 拼团活动列表

--- src/pages/activitys/group/index.tsx
+++ src/pages/activitys/group/index.tsx
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import TabPage from '@/components/tab-page';
-import { Flex, WingBlank } from 'antd-mobile';
+import { Flex, WingBlank, Toast } from 'antd-mobile';
 import styles from './index.less';
 import GroupItem from './item';
+import request from '@/services/request';
+import AddButton from '../components/add-button';
+import router from 'umi/router';
 
 export default class Group extends Component {
-  handleChange = (id: any) => {};
-  render() {
-    const tabs = [
-      { id: 0, label: '进行中' },
-      { id: 1, label: '待生效' },
-      { id: 2, label: '已结束' },
-    ];
-    return (
-      <TabPage tabs={tabs} onChange={this.handleChange}>
-        <Flex direction="column" style={{ height: '100%' }}>
-          <Flex.Item className={styles.pageContent}>
-            <WingBlank>
-              <GroupItem />
-            </WingBlank>
-          </Flex.Item>
-          <div className="add-icon">+</div>
-        </Flex>
-      </TabPage>
-    );
-  }
+	state = {
+		data: []
+	};
+	componentDidMount = () => this.getData(1);
+	getData = async (id?: any) => {
+		Toast.loading('');
+		const res = await request({ url: 'api/merchant/youhui/group_activity', params: { type: id } });
+		Toast.hide();
+		if (res.code === 200) {
+			this.setState({ data: res.data });
+		}
+	};
+	handleChange = (id: any) => this.getData(id);
+	handleClickAddButton = () => router.push('/activitys/group/createGroup');
+	handleClickGroup = (id: number) => {};
+	render() {
+		const tabs = [{ id: 1, label: '进行中' }, { id: 2, label: '待生效' }, { id: 3, label: '已结束' }];
+		const groups = this.state.data.map((_: any) => <GroupItem key={_.id} {..._} onClick={this.handleClickGroup} />);
+		return (
+			<TabPage tabs={tabs} onChange={this.handleChange}>
+				<WingBlank>{groups}</WingBlank>
+				<AddButton onClick={this.handleClickAddButton} />
+			</TabPage>
+		);
+	}
 }
