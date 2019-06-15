2a1562a178b8cbcdcd73a641885a306eb5ad34ce
domen <mixiaofeng1996@gmail.com>
2019-06-15 22:23:42 +0800

    feat: 样式修复

--- src/pages/my/give/index.tsx
+++ src/pages/my/give/index.tsx
@@ -5,33 +5,33 @@ import styles from './index.less';
 import MyGiveList from './list';
 
 export default class MyGive extends Component {
-  render() {
-    return (
-      <Flex className={styles.page} direction="column">
-        <Flex className="head" justify="center">
-          <Flex direction="column">
-            <div className="value">190999</div>
-            <span>当前可用</span>
-          </Flex>
-          <Flex direction="column">
-            <div className="value">190</div>
-            <span>冻结</span>
-          </Flex>
-        </Flex>
-        <Flex.Item>
-          <WingBlank style={{ height: '100%' }}>
-            <Flex className="page-content" direction="column">
-              <Flex className="content-head">
-                <img src="" alt="" />
-                <span>积分记录</span>
-              </Flex>
-              <Flex.Item>
-                <MyGiveList />
-              </Flex.Item>
-            </Flex>
-          </WingBlank>
-        </Flex.Item>
-      </Flex>
-    );
-  }
+	render() {
+		return (
+			<Flex className={styles.page} direction="column">
+				<Flex className="head" justify="center">
+					<Flex direction="column">
+						<div className="value">190999</div>
+						<span>当前可用</span>
+					</Flex>
+					<Flex direction="column">
+						<div className="value">190</div>
+						<span>冻结</span>
+					</Flex>
+				</Flex>
+				<Flex.Item>
+					<Flex className="page-content" direction="column">
+						<Flex className="content-head">
+							<img src={require('./icon.png')} alt="" />
+							<span>积分记录</span>
+						</Flex>
+						<Flex.Item>
+							<WingBlank style={{ height: '100%' }}>
+								<MyGiveList />
+							</WingBlank>
+						</Flex.Item>
+					</Flex>
+				</Flex.Item>
+			</Flex>
+		);
+	}
 }
