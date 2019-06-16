b0fbd1196c492e6577d96db0661e7030ff0eb8dd
domen <mixiaofeng1996@gmail.com>
2019-06-16 20:39:05 +0800

    feat: 积分

--- src/pages/my/give/item.tsx
+++ src/pages/my/give/item.tsx
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { Flex } from 'antd-mobile';
 import styles from './index.less';
 
-export default class MyGiveItem extends Component {
-  render() {
-    return (
-      <Flex className={styles.item}>
-        <Flex.Item>
-          <div>谁给他的礼品币</div>
-          <span>2018.12.20 18:00</span>
-        </Flex.Item>
-        <div className="after">
-          <div>+1000</div>
-          <span>可用</span>
-        </div>
-      </Flex>
-    );
-  }
+export default class MyGiveItem extends Component<any> {
+	render() {
+		return (
+			<Flex className={styles.item}>
+				<Flex.Item>
+					<div>{this.props.name}</div>
+					<span>{this.props.created_at}</span>
+				</Flex.Item>
+				<div className="after">
+					<div>{this.props.integral}</div>
+					<span>{this.props.msg}</span>
+				</div>
+			</Flex>
+		);
+	}
 }
