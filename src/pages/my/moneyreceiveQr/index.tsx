9c776906d1e12da7ad54a41cc97b412abe751626
songyizhe <769724661@qq.com>
2019-06-19 16:07:27 +0800

    冲突

---
+++ src/pages/my/moneyreceiveQr/index.tsx
@@ -0,0 +1,23 @@
+/**
+ * title: 我的收款码
+ */
+import React, {Component} from 'react';
+
+export default class MoneyReceiveQr extends Component {
+  state = {
+    list: []
+  };
+  componentDidMount (){
+    // this.setState({
+    //   list
+    // })
+  };
+
+  render (){
+    return (
+      <div>
+        1
+      </div>
+    )
+  }
+}
