05917e90f8e44ee2c66bbe27e8fa6a0311fa70ae
domen <mixiaofeng1996@gmail.com>
2019-06-19 15:53:23 +0800

    feat: 删除与暂停

--- src/pages/my/coupon/detail/detail.tsx
+++ src/pages/my/coupon/detail/detail.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import styles from './index.less';
 import request from '@/services/request';
-import { Toast } from 'antd-mobile';
+import { Toast, Flex } from 'antd-mobile';
 import MyCouponItem, { Item } from '../item';
+import router from 'umi/router';
 
 interface Props {
 	id: any;
@@ -30,12 +31,56 @@ export default class ContentDetail extends Component<Props> {
 
 	handleClickItem = () => {};
 
+	handleDeleteCoupon = async () => {
+		Toast.loading('');
+		const res = await request({ url: 'v3/coupons/' + this.props.id, method: 'delete' });
+		Toast.hide();
+		if (res.code === 200) {
+			Toast.success('删除成功');
+			router.goBack();
+		} else {
+			Toast.fail(res.data);
+		}
+	};
+
+	handleStopCoupon = async () => {
+		Toast.loading('');
+		const res = await request({ url: 'v3/coupons/stop/' + this.props.id, method: 'put' });
+		Toast.hide();
+		if (res.code === 200) {
+			Toast.success('暂停成功');
+			router.goBack();
+		} else {
+			Toast.fail(res.data);
+		}
+	};
+
 	render() {
 		const itemProps: Item = this.state.data;
+		const rules = '';
 		return (
-			<div className={styles.detail}>
-				<MyCouponItem {...itemProps} onClick={this.handleClickItem} />
-			</div>
+			<Flex direction="column" className={styles.detail}>
+				<Flex.Item>
+					<MyCouponItem {...itemProps} onClick={this.handleClickItem} />
+					<div className="couponDetail">
+						<Flex className="head">
+							<img src="" alt="" />
+							<Flex.Item>
+								<div className="title">使用规则</div>
+							</Flex.Item>
+						</Flex>
+						<ul className="ruleList">{rules}</ul>
+					</div>
+				</Flex.Item>
+				<Flex className="footerBtns">
+					<Flex.Item className="deleteBtn" onClick={this.handleDeleteCoupon}>
+						删除优惠券
+					</Flex.Item>
+					<Flex.Item className="stopBtn" onClick={this.handleStopCoupon}>
+						暂停发放优惠券
+					</Flex.Item>
+				</Flex>
+			</Flex>
 		);
 	}
 }
