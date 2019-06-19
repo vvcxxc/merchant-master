d719ca9dbfa4a9c52e9f5690b66a0ba9f1f7ec47
domen <mixiaofeng1996@gmail.com>
2019-06-19 23:05:44 +0800

    feat: 订单详情

--- src/pages/order/detail/index.tsx
+++ src/pages/order/detail/index.tsx
@@ -1,60 +1,75 @@
 /**title: 订单详情 */
 
 import React, { Component } from 'react';
-import { WingBlank, Flex } from 'antd-mobile';
+import { WingBlank, Flex, Toast } from 'antd-mobile';
 import styles from './index.less';
+import request from '@/services/request';
 
-export default class OrderDetail extends Component {
-  render() {
-    return (
-      <div className={styles.page}>
-        <WingBlank>
-          <div className="price">+499.99</div>
-          <div className="trade">交易成功</div>
-          <div className="content">
-            <div className="box">
-              <Flex>
-                <div className="label">订单金额</div>
-                <Flex.Item>519.99</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">创建时间</div>
-                <Flex.Item>2018-12-20 18:00</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">订单编号</div>
-                <Flex.Item>AB45213678914562</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">商户订单</div>
-                <Flex.Item>2018122034571234567894</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">优惠券</div>
-                <Flex.Item>兑换券</Flex.Item>
-              </Flex>
-            </div>
-            <div className="box">
-              <Flex>
-                <div className="label">支付用户</div>
-                <Flex.Item>用户名称</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">使用状态</div>
-                <Flex.Item>已使用</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">使用时间</div>
-                <Flex.Item>2019-05-09</Flex.Item>
-              </Flex>
-              <Flex>
-                <div className="label">实收金额</div>
-                <Flex.Item>100868元</Flex.Item>
-              </Flex>
-            </div>
-          </div>
-        </WingBlank>
-      </div>
-    );
-  }
+interface State {
+	data: any;
+}
+export default class OrderDetail extends Component<any, State> {
+	state: State = { data: {} };
+	componentDidMount = () => this.getData();
+	getData = async () => {
+		Toast.loading('');
+		const res = await request({ url: 'v3/coupons/order_info/' + this.props.location.query.id });
+		Toast.hide();
+		if (res.code === 200) {
+			this.setState({ data: res.data });
+		}
+	};
+	render() {
+		const data = this.state.data;
+		return (
+			<div className={styles.page}>
+				<WingBlank>
+					<div className="price">+{data.return_money}</div>
+					<div className="trade">交易成功</div>
+					<div className="content">
+						<div className="box">
+							<Flex>
+								<div className="label">订单金额</div>
+								<Flex.Item>{data.return_money}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">创建时间</div>
+								<Flex.Item>{data.create_time}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">订单编号</div>
+								<Flex.Item>{data.youhui_sn}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">商户订单</div>
+								<Flex.Item>{data.channel_order_sn}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">优惠券</div>
+								<Flex.Item>{data.youhui_type === 0 ? '兑换' : '优惠'}券</Flex.Item>
+							</Flex>
+						</div>
+						<div className="box">
+							<Flex>
+								<div className="label">支付用户</div>
+								<Flex.Item>{data.user_name}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">使用状态</div>
+								<Flex.Item>{data.status}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">使用时间</div>
+								<Flex.Item>{data.refund_time}</Flex.Item>
+							</Flex>
+							<Flex>
+								<div className="label">实收金额</div>
+								<Flex.Item>{data.pay_money}元</Flex.Item>
+							</Flex>
+						</div>
+					</div>
+				</WingBlank>
+			</div>
+		);
+	}
 }
