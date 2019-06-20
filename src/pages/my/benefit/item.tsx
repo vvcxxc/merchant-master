20372584a8e19fa61905fff3072ccd73dece0724
domen <mixiaofeng1996@gmail.com>
2019-06-20 22:38:16 +0800

    faet: 收益

--- src/pages/my/benefit/item.tsx
+++ src/pages/my/benefit/item.tsx
@@ -9,16 +9,17 @@ export interface Item {
 	payment_type: number;
 	amount: string;
 	store_amount: string;
+	small_icon: string;
 }
 
 export default class BenefitItem extends Component<Item> {
 	render() {
 		return (
 			<Flex className={styles.item}>
-				<img src="" className="img" alt="" />
+				<img src={this.props.small_icon} className="img" alt="" />
 				<Flex.Item>
 					<Flex className="title">
-						{this.props.order_sn}
+						<Flex.Item>{this.props.order_sn}</Flex.Item>
 						<div className="price">{this.props.amount}</div>
 					</Flex>
 					<div className="info">{this.props.create_time}</div>
