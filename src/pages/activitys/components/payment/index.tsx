018ae84e7cf2ccf077a034d0b6c925ad19d25e19
songyizhe <769724661@qq.com>
2019-06-20 10:49:14 +0800

    支付邮费

--- src/pages/activitys/components/payment/index.tsx
+++ src/pages/activitys/components/payment/index.tsx
@@ -5,19 +5,55 @@
 import React, { Component } from 'react';
 import { Flex, WingBlank, Button} from 'antd-mobile';
 import styles from './index.less';
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 interface Props {
   list: any
 }
 
 export default class PayMent extends Component<Props> {
+  openId = Cookies.get('test_open_id') || 'oy6pQ0yY_L34r_vcMkCNHJPk-iCk';
   state = {
-    list: {}
+    list: {
+      money: 0,
+      number: 0,
+      order_sn: '',
+      payMoney: 0
+    }
   };
   componentDidMount (){
     this.setState({
       list: this.props.list
     })
+  }
+
+  submit = async() => {
+    let { list } = this.state;
+    const res = await axios({
+      url: 'http://test.api.tdianyi.com/payCentre/toSupplierWxPay',
+      method: 'post',
+      headers: {
+        Authorization: localStorage.getItem('token')
+      },
+      data: {
+        type: '10',
+        public_type_id: list.order_sn,
+        number: list.number,
+        open_id: this.openId,
+        xcx: '0'
+      }
+    });
+    let {data, code} = res.data;
+    if(code == 200){
+      window.WeixinJSBridge.invoke('getBrandWCPayRequest', data, function(res: { err_msg: string }) {
+				``;
+				if (res.err_msg == 'get_brand_wcpay_request:ok') {
+					// '支付成功'
+				}
+			});
+    }
+
   }
 
   render (){
@@ -28,25 +64,25 @@ export default class PayMent extends Component<Props> {
         <WingBlank style={{marginTop: 84}}>
           <Flex className={styles.list}>
             <div>邮费</div>
-            <div style={{fontSize: '23px', color: '#999999'}}>{this.state.list.money}元</div>
+            <div style={{fontSize: '0.23rem', color: '#999999'}}>{this.state.list.money}元</div>
           </Flex>
           <Flex className={styles.list}>
             <div>数量</div>
-            <div style={{fontSize: '23px', color: '#999999'}}>{this.state.list.number}张</div>
+            <div style={{fontSize: '0.23rem', color: '#999999'}}>{this.state.list.number}张</div>
           </Flex>
           <Flex className={styles.pay_type}>
             支付方式
           </Flex>
           <Flex className={styles.wechat_type}>
-            <Flex>
+            <Flex className={styles.flex}>
               <img src={require('./wechat.png')}/>
               <span>微信支付</span>
             </Flex>
-            <Flex>
+            <Flex className={styles.flex}>
               <img src={require('./choose.png')}/>
             </Flex>
           </Flex>
-          <Button type="primary" style={{ marginTop: 187 }}>
+          <Button type="primary" style={{ marginTop: 187 }} onClick={this.submit}>
             提交订单
           </Button>
         </WingBlank>
