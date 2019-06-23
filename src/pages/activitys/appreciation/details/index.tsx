b0bc447a092dfcc1c22843fcdb36514487032c65
songyizhe <769724661@qq.com>
2019-06-23 15:34:36 +0800

    活动详情页

--- src/pages/activitys/appreciation/details/index.tsx
+++ src/pages/activitys/appreciation/details/index.tsx
@@ -8,26 +8,25 @@ export default class GroupDetails extends Component {
   state = {
     info: {
       activity_image: '',
-      group_count: {
-        group_defeated: '',
-        group_underway: '',
-        group_success: '',
+      appreciation_count: {
+        participate_number: '',
+        participation_number: '',
         coupons_number: ''
       },
-      group_info: {
+      appreciation_info: {
         activity_type: '',
-        pay_money: '',
-        group_money: '',
-        group_number: '',
+        max_money: '',
+        appreciation_number: '',
+        participation_number: '',
         activity_time: ''
       },
-      group_coupons_info: {
+      appreciation_coupons_info: {
         market_price: '',
         use_tim: '',
-        coupons_number: '',
+        coupons_is_number: '',
         description: []
       },
-      group_gif_info: {
+      appreciation_gif_info: {
         gif_name: '',
         gif_pic: '',
         gif_integral: '',
@@ -35,21 +34,22 @@ export default class GroupDetails extends Component {
       }
     },
     id: ''
-
   }
   componentDidMount (){
+    this.setState({id: this.props.location.query.id})
     request({
-      url: 'api/merchant/youhui/group_info',
+      url: 'api/merchant/youhui/getAppreciationInfo',
       method: 'get',
       params: {
         coupons_activity_log_id: this.props.location.query.id
       }
     }).then(res => {
       let {data} = res;
+      // console.log(res)
       this.setState({info: data})
     })
   }
-
+  
   stop = () => {
     request({
       url: 'api/merchant/youhui/appreciation/activity/stop/'+this.state.id,
@@ -64,7 +64,9 @@ export default class GroupDetails extends Component {
 
   render (){
     const { info } = this.state;
-    const description = info.group_coupons_info.description.map((item,idx) => <p key={idx}>· {item}</p>)
+    const description = info.appreciation_coupons_info.description.map((item,idx) => <p key={idx}>· {item}</p>)
+
+
 
     return (
       <div className={styles.detailsPage}>
@@ -78,11 +80,6 @@ export default class GroupDetails extends Component {
             <img src={require('./share.png')}/>
           </Flex>
 
-          {/* 图片 */}
-          <Flex className={styles.activity_img}>
-            <img src={info.activity_image}/>
-          </Flex>
-
           {/* 基本信息 */}
           <Flex className={styles.title}>
             <div className={styles.gang}>{null}</div>
@@ -90,23 +87,23 @@ export default class GroupDetails extends Component {
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>活动类型：</div>
-            <div className={styles.item_detail}>{info.group_info.activity_type}</div>
+            <div className={styles.item_detail}>{info.appreciation_info.activity_type}</div>
           </Flex>
           <Flex className={styles.item} align='start'>
-            <div className={styles.item_name}>原价：</div>
-            <div className={styles.item_detail}>{info.group_info.pay_money}元</div>
+            <div className={styles.item_name}>封顶值：</div>
+            <div className={styles.item_detail}>{info.appreciation_info.max_money}元</div>
           </Flex>
           <Flex className={styles.item} align='start'>
-            <div className={styles.item_name}>拼团价：</div>
-            <div className={styles.item_detail}>{info.group_info.group_money}元</div>
+            <div className={styles.item_name}>增值人数：</div>
+            <div className={styles.item_detail}>{info.appreciation_info.appreciation_number}人</div>
           </Flex>
           <Flex className={styles.item} align='start'>
-            <div className={styles.item_name}>拼团人数：</div>
-            <div className={styles.item_detail}>{info.group_info.group_number}人</div>
+            <div className={styles.item_name}>总参与人数：</div>
+            <div className={styles.item_detail}>{info.appreciation_info.participation_number}人</div>
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>活动时间：</div>
-            <div className={styles.item_detail}>{info.group_info.activity_time} </div>
+            <div className={styles.item_detail}>{info.appreciation_info.activity_time}</div>
           </Flex>
 
           {/* 优惠券信息 */}
@@ -116,19 +113,21 @@ export default class GroupDetails extends Component {
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>市场价：</div>
-            <div className={styles.item_detail}>{info.group_coupons_info.market_price}元</div>
+            <div className={styles.item_detail}>{info.appreciation_coupons_info.market_price}元</div>
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>使用时间：</div>
-            <div className={styles.item_detail}>{info.group_coupons_info.use_tim}</div>
+            <div className={styles.item_detail}>{info.appreciation_coupons_info.use_tim}</div>
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>数量：</div>
-            <div className={styles.item_detail}>{info.group_coupons_info.coupons_number}张</div>
+            <div className={styles.item_detail}>{info.appreciation_coupons_info.coupons_is_number}张</div>
           </Flex>
           <Flex className={styles.item_height} align='start'>
             <div className={styles.item_name}>使用须知：</div>
             <div className={styles.item_long}>
+              {/* <p>· 消费高峰期时可能需要等</p> */}
+              {/* <p>· 消费高峰期时可能需要等</p> */}
               {/* <p>· 消费高峰期时可能需要等</p> */}
               {description}
             </div>
@@ -141,28 +140,28 @@ export default class GroupDetails extends Component {
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>礼品名称：</div>
-            <div className={styles.item_detail}>{info.group_gif_info.gif_name}</div>
+            <div className={styles.item_detail}>{info.appreciation_gif_info.gif_name}</div>
           </Flex>
           <Flex className={styles.item_height} align='start'>
             <div className={styles.item_name}>礼品图片：</div>
             <div className={styles.item_img}>
-              <img src={info.group_gif_info.gif_pic}/>
+              <img src={info.appreciation_gif_info.gif_pic}/>
             </div>
           </Flex>
           <Flex className={styles.item} align='start'>
             <div className={styles.item_name}>所需积分：</div>
-            <div className={styles.item_detail}>{info.group_gif_info.delivery}积分</div>
+            <div className={styles.item_detail}>{info.appreciation_gif_info.delivery}积分</div>
           </Flex>
           <Flex className={styles.item_height} align='start'>
             <div className={styles.item_name}>配送方式：</div>
             <div className={styles.item_long}>
-              <p>{info.group_gif_info.gif_integral}</p>
+              <p>{info.appreciation_gif_info.gif_integral}</p>
               {/* <p>邮寄 邮费谁出</p> */}
             </div>
           </Flex>
 
           {/* 撤销按钮 */}
-          <Button type='primary' style={{marginTop: 50, marginBottom: 30}}>撤销活动</Button>
+          <Button type='primary' style={{marginTop: 50, marginBottom: 30}} onClick={this.stop}>撤销活动</Button>
 
         </WingBlank>
       </div>
