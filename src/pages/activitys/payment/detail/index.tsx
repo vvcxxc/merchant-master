e89910de3e539ad0786260f54de565941302d764
domen <mixiaofeng1996@gmail.com>
2019-06-27 15:04:49 +0800

    feat: 删除

--- src/pages/activitys/payment/detail/index.tsx
+++ src/pages/activitys/payment/detail/index.tsx
@@ -2,10 +2,11 @@
 
 import React, { Component } from 'react';
 import request from '@/services/request';
-import { Toast, Flex, WingBlank } from 'antd-mobile';
+import { Toast, Flex, WingBlank, Button } from 'antd-mobile';
 import styles from './index.less';
 import CouponCard from './coupon';
 import moment from 'moment';
+import router from 'umi/router';
 
 export default class payReturnDetail extends Component<any> {
 	state = {
@@ -21,6 +22,15 @@ export default class payReturnDetail extends Component<any> {
 			this.setState({ data: res.data, hasData: true });
 		}
 	};
+	handleDelete = async () => {
+		Toast.loading('');
+		const res = await request({ url: 'v3/return_coupons/' + this.props.location.query.id, method: 'delete' });
+		Toast.hide();
+		if (res.code === 200) {
+			Toast.success('删除成功');
+			setTimeout(router.goBack, 1000);
+		}
+	};
 	render() {
 		const data: any = this.state.data;
 
@@ -34,19 +44,28 @@ export default class payReturnDetail extends Component<any> {
 		));
 
 		return (
-			<div className={styles.page}>
-				<WingBlank>
-					<Flex className="head">
-						<div className="label">返</div>
-						<Flex.Item className="name">{data.activity.name}</Flex.Item>
-						<div className="time">
-							{moment.unix(data.activity.activity_begin_time).format('YYYY/MM/DD')}-
-							{moment.unix(data.activity.activity_end_time).format('YYYY/MM/DD')}
-						</div>
-					</Flex>
-					{coupons}
-				</WingBlank>
-			</div>
+			<Flex className={styles.page} align="start" direction="column">
+				<Flex.Item>
+					<WingBlank>
+						<Flex className="head">
+							<div className="label">返</div>
+							<Flex.Item className="name">{data.activity.name}</Flex.Item>
+							<div className="time">
+								{moment.unix(data.activity.activity_begin_time).format('YYYY/MM/DD')}-
+								{moment.unix(data.activity.activity_end_time).format('YYYY/MM/DD')}
+							</div>
+						</Flex>
+						{coupons}
+					</WingBlank>
+				</Flex.Item>
+				<div className="buttonWrap">
+					<WingBlank>
+						<Button type="primary" className="deleteBtn" onClick={this.handleDelete}>
+							删除活动
+						</Button>
+					</WingBlank>
+				</div>
+			</Flex>
 		);
 	}
 }
