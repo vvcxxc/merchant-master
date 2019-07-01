cbd291f7903a4f6bb5d6fb40e10650cdbf369985
songyizhe <769724661@qq.com>
2019-07-01 14:50:46 +0800

    修改授权接口

--- .umirc.release.ts
+++ .umirc.release.ts
@@ -4,7 +4,7 @@ const config: IConfig = {
   define: {
     "window.api": "http://release.api.supplier.tdianyi.com/",
     "window.open_id": "open_id",
-    "window.url": "http://release.api.tdianyi.com/",
+    "window.url": "http://release.api.supplier.tdianyi.com/",
     "window.from": "http://release.supplierv2.tdianyi.com/",
     "window.pay_url": "http://release.api.tdianyi.com/payCentre/toSupplierWxPay"
   }
