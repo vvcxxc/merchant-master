05577d0d0394bbd769c4130016648d53d28fc4dd
songyizhe <769724661@qq.com>
2019-07-01 16:02:20 +0800

    测试12.

--- src/pages/my/coupon/detail/list.tsx
+++ src/pages/my/coupon/detail/list.tsx
@@ -33,18 +33,6 @@ export default class ReceiveList extends Component<Props> {
 		}
 	};
 	render() {
-<<<<<<< HEAD
-		const list = this.state.list.map((_: Item) => (
-			<Flex className="item">
-				<img className="headImg" src={_.image} alt="" />
-				<Flex.Item>
-					<Flex className="title">
-						<Flex.Item>{_.user_name}</Flex.Item>
-						<span className={_.status === 2 ? 'yellow' : ''}>{_.status_msg}</span>
-					</Flex>
-					<div className="time">{_.create_time}</div>
-				</Flex.Item>
-=======
 		const list = this.state.list.length ? (
 			this.state.list.map((_: Item) => (
 				<Flex className="item">
@@ -62,7 +50,6 @@ export default class ReceiveList extends Component<Props> {
 			<Flex direction="column" justify="center" className="noData">
 				<img src={require('./noData.png')} />
 				<span>暂无领取记录</span>
->>>>>>> release
 			</Flex>
 		);
 		return <div className={styles.receiceList}>{list}</div>;
