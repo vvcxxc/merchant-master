730d2a737226e6e2fffcc7a95470db5ef576df46
songyizhe <769724661@qq.com>
2019-07-01 15:44:51 +0800

    授权

--- .umirc.master.ts
+++ .umirc.master.ts
@@ -4,7 +4,7 @@ const config: IConfig = {
   define: {
     "window.api": "http://api.supplier.tdianyi.com/",
     "window.open_id": "open_id",
-    "window.url": "http://api.supplier.tdianyi.com/",
+    "window.url": "http://release.api.tdianyi.com/",
     "window.from": "http://supplier.tdianyi.com",
     "window.pay_url": "http://api.tdianyi.com/payCentre/toSupplierWxPay"
   },
