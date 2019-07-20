25c129fa4b5ade853de52927b5a936b1ad34e265
songyizhe <769724661@qq.com>
2019-07-20 09:21:13 +0800

    暂时隐藏部分功能

--- src/pages/ad/components/expense-calendar/index.tsx
+++ src/pages/ad/components/expense-calendar/index.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 export default class ExpenseCalendar extends Component<Props> {
 	render() {
-		const tableItems = this.props.log.map(_ => (
+		const tableItems = this.props.log ? this.props.log.map(_ => (
 			<Flex className={styles.tableItem} key={_.create_time}>
 				<Flex.Item>{moment.unix(_.create_time).format('YYYY-MM-DD')}</Flex.Item>
 				<span>{_.info}</span>
 				<Flex.Item>{_.money}</Flex.Item>
 			</Flex>
-		));
+		)) : null;
 		{
 			/* <Flex className={styles.totalExpense} justify="end">
 					总消耗：1.4
