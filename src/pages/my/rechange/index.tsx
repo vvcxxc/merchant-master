0bdc3a5e4112e981cd83b697f16c101da639fdd6
songyizhe <769724661@qq.com>
2019-08-09 10:31:02 +0800

    授权问题

--- src/pages/my/rechange/index.tsx
+++ src/pages/my/rechange/index.tsx
@@ -6,55 +6,76 @@ import { WingBlank, Flex, InputItem, Button, Toast } from 'antd-mobile';
 import request from '@/services/request';
 import Cookies from 'js-cookie';
 declare global {
-  interface Window { open_id: string; pay_url: string;}
+  interface Window { open_id: string; pay_url: string; Url: string }
 }
+const Url = window.url ? window.url : 'http://test.api.tdianyi.com/';
 const open_id = window.open_id ? window.open_id : 'test_open_id';
 export default class Rechange extends Component {
 
-	state = { money: 0 };
-	/**input change */
-	handleInputChange = (value: any) => this.setState({ money: parseFloat(value) });
-	/** recahnge submit value */
-	submit = async () => {
+  state = { money: 0 };
+  /**input change */
+  handleInputChange = (value: any) => this.setState({ money: parseFloat(value) });
+  /** recahnge submit value */
+  submit = async () => {
     let openId = Cookies.get(open_id)
-		Toast.loading('充值中');
-		const res = await request({
-			url: 'v3/pay/recharge',
-			method: 'post',
-			data: {
-				xcx: 0,
-				rechargeMoney: this.state.money,
-				open_id: openId
-			}
-		});
+    // 判断是否授权
+    if (openId) {
+      Toast.loading('充值中');
+      const res = await request({
+        url: 'v3/pay/recharge',
+        method: 'post',
+        data: {
+          xcx: 0,
+          rechargeMoney: this.state.money,
+          open_id: openId
+        }
+      });
 
-		Toast.hide();
+      Toast.hide();
 
-		if (res.code === 200) {
-			window.WeixinJSBridge.invoke('getBrandWCPayRequest', res.data, function(res: { err_msg: string }) {
-				``;
-				if (res.err_msg == 'get_brand_wcpay_request:ok') {
-					// '支付成功'
-				}
-			});
-		}
-	};
-	render() {
-		return (
-			<div className={styles.page}>
-				<WingBlank>
-					<div className="title">充值金额</div>
-					<Flex className="input-wrap">
-						<span className="symbol">￥</span>
-						<Flex.Item>
-							<InputItem type="money" placeholder="" onChange={this.handleInputChange} clear/>
-						</Flex.Item>
-					</Flex>
-					<Button type="primary" onClick={this.submit}>
-						充值
+      if (res.code === 200) {
+        window.WeixinJSBridge.invoke('getBrandWCPayRequest', res.data, function (res: { err_msg: string }) {
+          ``;
+          if (res.err_msg == 'get_brand_wcpay_request:ok') {
+            // '支付成功'
+          }
+        });
+      }
+    }else {
+      this.auth()
+    }
+
+  };
+
+  // 授权
+  auth = () => {
+    let from = window.location.href;
+    let url = Url + 'wechat/wxoauth?code_id=0&from=' + from;
+    url = encodeURIComponent(url);
+    let urls =
+      'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri=' +
+      url +
+      '&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE';
+    return (window.location.href = urls);
+  }
+
+
+  render() {
+    return (
+      <div className={styles.page}>
+        <WingBlank>
+          <div className="title">充值金额</div>
+          <Flex className="input-wrap">
+            <span className="symbol">￥</span>
+            <Flex.Item>
+              <InputItem type="money" placeholder="" onChange={this.handleInputChange} clear />
+            </Flex.Item>
+          </Flex>
+          <Button type="primary" onClick={this.submit}>
+            充值
 					</Button>
-				</WingBlank>
-			</div>
-		);
-	}
+        </WingBlank>
+      </div>
+    );
+  }
 }
