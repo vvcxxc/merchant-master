d308e1c31f2a66ae84a9c1132fa326f1f660b24a
xiaoqilin <2597462505@qq.com>
2019-08-21 15:28:19 +0800

    feat:遮挡层加一个箭头

--- src/pages/activitys/appreciation/componts/bottom_share/index.tsx
+++ src/pages/activitys/appreciation/componts/bottom_share/index.tsx
@@ -13,7 +13,8 @@ interface Props {
 export default class BottomShare extends Component<Props>{
   state = {
     showShare: false,
-    shareButton:false
+    shareButton: false,
+    showBottom:true//控制底部用户操作部分
   }
 
 
@@ -38,14 +39,16 @@ export default class BottomShare extends Component<Props>{
 
   //点击分享
   shareData = () => {
+    // 点击分享的时候 遮挡层不能消失 只消失分享 海报 取消部分
+    this.setState({ showBottom: false })
+    
     let code :any = this.props.type
     let meta: any = {
       ['增值']: code.id
     }
 
-    this.setState({ showShare: true }) // 控制关闭分享组件
-    this.props.closeShare(false)
-
+    // this.setState({ showShare: true }) // 控制关闭分享组件
+    // this.props.closeShare(false)
     let userAgent = navigator.userAgent;
     let isIos = userAgent.indexOf('iPhone') > -1;
     let url: any;
@@ -70,25 +73,28 @@ export default class BottomShare extends Component<Props>{
         signature: res.signature,
         jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData']
       });
-      this.setState({ showShare: true }) // 控制关闭分享组件
+      // this.setState({ showShare: true }) // 控制关闭分享组件
       wx.ready(() => {
         wx.updateAppMessageShareData({
-          title: '伊哲大逗逼',
+          title: '伊哲要上天',
           link: 'http://test.mall.tdianyi.com/#/pages/business/index?id='+meta[code.name],//这个id从哪里来
           imgUrl: '../../icon.png',
           success: function () {
             alert('成功了')
+            //点击分享后 出现遮挡层 然后把
           }
         })
       })
     })
   }
   
+  
 
   render() {
     return (
-      <div style={{ display: this.props.showShare ? '':'none'}} className={styles.keep_out}>
-        <div className={styles.share_box}>
+      <div style={{ display: this.props.showShare ? '' : 'none' }} className={styles.keep_out}>
+        <img className={styles.keep_out_img} src={require('../../../../../assets/jiantou.png')}></img>
+        <div className={styles.share_box} style={{ display: this.state.showBottom ? '' : 'none' }}>
           <div className={styles.box}>
             <div className={styles.all_center} onClick={this.shareData}>
               <img src={require('../../../../../assets/share.png')} alt="" />
