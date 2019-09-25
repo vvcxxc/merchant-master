405adc59caca90d0ff75db6c586a6177a2ccd27b
xiaoqilin <2597462505@qq.com>
2019-09-25 15:53:56 +0800

    feat:'

--- src/pages/activitys/appreciation/details/index.tsx
+++ src/pages/activitys/appreciation/details/index.tsx
@@ -107,8 +107,13 @@ export default connect(({ activity }: any) => activity)(
           git_money: data.appreciation_gif_info.gif_integral,//礼品金额
           gif_pic: data.appreciation_gif_info.gif_pic,//礼品图片
           gift_id: data.appreciation_gif_info.gift_id,// 礼品id 如果为0 海报就不显示礼品图片以及信息
-          pay_money: data.appreciation_info.max_money,  // 后端改变上下参数
-          max_money: data.appreciation_info.pay_money,
+          // pay_money: data.appreciation_info.max_money,  // 后端改变上下参数
+          // max_money: data.appreciation_info.pay_money,
+          // pay_money: data.appreciation_info.pay_money,  // 后端改变上下参数
+          // max_money: data.appreciation_info.poster_max_money,
+          pay_money: data.appreciation_info.pay_money,
+          max_money: data.appreciation_info.poster_max_money,
+          poster_max_money: data.appreciation_info.poster_max_money,
           ...data.supplier,
           use_tim: data.appreciation_coupons_info.use_tim,
           gif_name: data.appreciation_gif_info.gif_name,
