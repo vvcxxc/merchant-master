4f249bb1c5fb03f9b120ae948302deffbff70e19
xiaoqilin <2597462505@qq.com>
2019-10-18 11:39:46 +0800

    amend:拼团详情修改字段

--- src/pages/activitys/group/member_information/index.tsx
+++ src/pages/activitys/group/member_information/index.tsx
@@ -89,7 +89,7 @@ export default class MemberInformation extends Component<Props> {
     const { myData,show } = this.state
     const { group_id, status } = this.props.location.query
     const specific: any = {
-      [1]: '未成团',
+      [1]: '卡券待生成',
       [2]: '已参团',
       [3]: '已过期'
     }
