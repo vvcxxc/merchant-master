8fa39180138bbc1393107674d75d8533fb71b2b6
vvcxxc <asd123>
2019-11-01 17:25:59 +0800

    测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -185,7 +185,7 @@ export default connect(({ app }: any) => app)(
 							}
 						}).then(res => {
 							if (res.code == 200) {
-								Toast.success(res.message, 1500, () => {
+								Toast.success(res.message, 2, () => {
 									router.push({
 										pathname: '/verificationPrize',
 									})
