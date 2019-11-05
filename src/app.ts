15f37705afa204b5816d49312b76fb1e13be45d3
xiqoqilin <w3c_2014@163.com>
2019-11-05 10:18:36 +0800

    ios10

--- src/app.ts
+++ src/app.ts
@@ -15,23 +15,23 @@ export const dva = {
 };
 
 const vConsole = new Vconsole()
-window.onerror = function (msg, url, line, col, error) {
-  console.log('123')
-  if (error) {
-    console.log(error)
-    const pattern = /Loading chunk (\d)+ failed/g;
-    // const pattern = /Cannot declare a let/g;
-    const isChunkLoadFailed = error.message.match(pattern);
-    console.log(isChunkLoadFailed)
-    alert(isChunkLoadFailed)
-    if(isChunkLoadFailed) {
-      location.reload()
-      console.log('触发了')
-    }
-    // location.reload()
-    // console.log('触发了')
-  }
-}
+// window.onerror = function (msg, url, line, col, error) {
+//   console.log('123')
+//   if (error) {
+//     console.log(error)
+//     const pattern = /Loading chunk (\d)+ failed/g;
+//     // const pattern = /Cannot declare a let/g;
+//     const isChunkLoadFailed = error.message.match(pattern);
+//     console.log(isChunkLoadFailed)
+//     alert(isChunkLoadFailed)
+//     if(isChunkLoadFailed) {
+//       location.reload()
+//       console.log('触发了')
+//     }
+//     // location.reload()
+//     // console.log('触发了')
+//   }
+// }
 /**路由变化 */
 export const onRouteChange = (params: { location: any, routes: any }) => {
   try {
