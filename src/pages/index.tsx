d91f11495233f4a6137222b72fb4a0ed1b5692ec
songyizhe <769724661@qq.com>
2019-11-15 17:33:38 +0800

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,7 +100,6 @@ export default connect(({ app }: any) => app)(
         }
 
         componentDidMount() {
-            console.log(aaa)
             let openId = Cookies.get(open_id);
             if (process.env.NODE_ENV != 'development') {
                 if (!openId) {
