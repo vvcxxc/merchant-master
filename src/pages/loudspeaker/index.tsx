a075f7188a9badcd81e07be19c8e39198c5e4b10
vvcxxc <asd123>
2019-11-26 10:25:43 +0800

    云音响

---
+++ src/pages/loudspeaker/index.tsx
@@ -0,0 +1,13 @@
+/**title: 我的云音响 */
+import React, { Component } from 'react';
+
+export default class Loudspeaker extends Component {
+    render() {
+        return (
+            <div >
+                1111111111111
+				</div>
+        );
+    }
+}
+
