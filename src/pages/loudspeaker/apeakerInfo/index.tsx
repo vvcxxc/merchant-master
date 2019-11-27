5ddceaaac98021ab1debe5587cde49462c928d24
xiaoqilin <2597462505@qq.com>
2019-11-27 15:18:16 +0800

    amend:轮播页面改变

--- src/pages/loudspeaker/apeakerInfo/index.tsx
+++ src/pages/loudspeaker/apeakerInfo/index.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Carousel, WingBlank } from 'antd-mobile';
 import styles from './index.less'
 import CloudSpeakers from './component/cloud_speakers'
+import wx from 'weixin-js-sdk';
+import request from '@/services/request';
 export default class qlPage extends Component {
 
   state = {
@@ -9,6 +11,36 @@ export default class qlPage extends Component {
     slideIndex: 0,
     imgHeight: 'auto',
     ApeakerlogisticsContentShow: false
+  }
+
+  componentWillMount() {
+    let userAgent = navigator.userAgent;
+    let isIos = userAgent.indexOf('iPhone') > -1;
+    let url: any;
+    if (isIos) {
+      url = sessionStorage.getItem('url');
+    } else {
+      url = location.href;
+    }
+    request({
+      url: 'wechat/getShareSign',
+      method: 'get',
+      params: {
+        url
+      }
+    }).then(res => {
+      console.log('diu', res);
+      wx.config({
+        debug: false,
+        appId: res.appId,
+        timestamp: res.timestamp,
+        nonceStr: res.nonceStr,
+        signature: res.signature,
+        jsApiList: ['makePhoneCall']
+      });
+    }).catch(err => {
+      console.log(err)
+    });
   }
   componentDidMount() {
     setTimeout(() => {
@@ -18,8 +50,13 @@ export default class qlPage extends Component {
     }, 10000);
   }
 
-  beforeChangeData = (form, to) => {
-    console.log('得到的', form, to);
+
+  CustomerServicePhone = () => {
+    wx.makePhoneCall({
+      phoneNumber: 15678787877
+    })
+      .then((res: any) => {
+      })
 
   }
 
@@ -31,7 +68,7 @@ export default class qlPage extends Component {
             查看物流
           </span>
         </div>
-        <WingBlank>
+        {/* <WingBlank>
           <Carousel className="space-carousel"
             frameOverflow="visible"
             dots={false}
@@ -43,7 +80,7 @@ export default class qlPage extends Component {
             selectedIndex={0}//当前索引
             swipeSpeed={22}
             autoplayInterval={50000000}
-            beforeChange={this.beforeChangeData.bind(this)}
+            // beforeChange={this.beforeChangeData.bind(this)}
             afterChange={index => this.setState({ slideIndex: index })}
           >
             {this.state.data.map((val, index) => (
@@ -57,7 +94,12 @@ export default class qlPage extends Component {
               </div>
             ))}
           </Carousel>
-        </WingBlank>
+        </WingBlank> */}
+        {
+
+            <CloudSpeakers height={true}></CloudSpeakers>
+
+        }
 
         {
           this.state.ApeakerlogisticsContentShow ? <div className={styles.ApeakerlogisticsContent} >
@@ -115,7 +157,7 @@ export default class qlPage extends Component {
             </div>
           </div> : null
         }
-        <div className={styles.customer_service}>客服电话-123654586</div>
+        <div className={styles.customer_service} onClick={this.CustomerServicePhone}>客服电话-123654586</div>
       </div>
     )
   }
