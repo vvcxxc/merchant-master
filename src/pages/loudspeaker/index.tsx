ef8e8f53b1f1fac39a19cb7d99f78ddcef640685
vvcxxc <asd123>
2019-11-27 10:45:06 +0800

    提交

--- src/pages/loudspeaker/index.tsx
+++ src/pages/loudspeaker/index.tsx
@@ -1,10 +1,11 @@
-/**title: 我的云音响 */
+/**title: 我的云音箱 */
 import React, { Component } from 'react';
+import router from 'umi/router';
 
 export default class Loudspeaker extends Component {
     render() {
         return (
-            <div >
+            <div onClick={()=>{router.push('/loudspeaker/apeakerInfo')}}>
                 1111111111111
 				</div>
         );
