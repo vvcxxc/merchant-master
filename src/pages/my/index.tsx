c7971b32c4cd4dd70d803c9d980809eee9cb4987
vvcxxc <asd123>
2019-12-03 09:52:22 +0800

    开放云音响入口

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -221,7 +221,7 @@ export default connect()(
 						>
 							抽奖核销记录
 						</Item>
-						{/* <Item
+						<Item
 							arrow="horizontal"
 							thumb={require('@/assets/my/cloud_voice_box.png')}
 							multipleLine
@@ -229,7 +229,7 @@ export default connect()(
 							className={styles.my_items}
 						>
 							我的云音箱
-						</Item> */}
+						</Item>
 						{signCode}
 					</List>
 					<List className={styles.my_info_items}>
