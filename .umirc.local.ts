07f0ecc654d4e46e813a7e4d29335a074efa7c4d
songyizhe <769724661@qq.com>
2020-01-16 14:56:27 +0800

    a

--- .umirc.local.ts
+++ .umirc.local.ts
@@ -2,8 +2,8 @@ import { IConfig } from "umi-types";
 
 const config: IConfig = {
   define: {
-    // "window.api": "http://test.api.supplier.tdianyi.com/",
-    "window.api": "http://release.api.supplier.tdianyi.com/",
+    "window.api": "http://test.api.supplier.tdianyi.com/",
+    // "window.api": "http://release.api.supplier.tdianyi.com/",
     "window.speaker_api": "http://test.api.voice.tdianyi.com/",
     "window.open_id": "test_open_id",
     "window.url": "http://test.api.tdianyi.com/",
