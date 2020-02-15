bc203ef8937a1042870ecab35624f0a945eac9e1
vvcxxc <asd123>
2020-02-15 14:03:46 +0800

    减号

--- src/pages/finance/detail/index.tsx
+++ src/pages/finance/detail/index.tsx
@@ -35,7 +35,7 @@ export default class Details extends Component {
         { order: '支付用户', value: res.data.user_name },
         { order: '订单金额', value: res.data.amount },
         {
-          order: '优惠总金额', value: sum.toFixed(2),
+          order: '优惠总金额', value: '-' + String(sum.toFixed(2)),
           children: res.data.youhui_info
         },
         { order: '交易手续费', value: res.data.service_amount },
@@ -92,7 +92,7 @@ export default class Details extends Component {
                           item.children.map((item2: any, index: number) => {
                             return <li key={index}>
                               <span>{item2.youhui_name}</span>
-                              <span>{Number(item2.youhui_money).toFixed(2)}</span>
+                              <span>{'-' + String(Number(item2.youhui_money).toFixed(2))}</span>
                             </li>
                           })
                         }
