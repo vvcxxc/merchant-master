80a8a7d6758184282405c8fb5b746d293d3501aa
xiqoqilin <w3c_2014@163.com>
2020-03-26 17:53:10 +0800

    配送

--- src/pages/activitys/dispatching/List/index.tsx
+++ src/pages/activitys/dispatching/List/index.tsx
@@ -1,100 +1,74 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styles from './index.less';
+import Request from '@/services/request';
+import router from 'umi/router';
 
 class List extends Component {
+
+    state = {
+        listData: []
+    }
+
+    componentWillMount = () => {
+        Request({
+            url: 'v3/merchant/delivery/order',
+        }).then(res => {
+            if (res.code == 200) {
+                this.setState({
+                    listData: res.data.data
+                })
+            }
+        })
+    }
+
+    handleDetail = (id:any) => {
+        router.push({
+            pathname: "/activitys/dispatching/detail",
+            query: {
+                id
+            }
+        })
+    }
+
     render() {
-        return(
+        const { listData } = this.state;
+        return (
             <div className={styles.dispatch_list}>
-                <div className={styles.list_item}>
-                    <div className={styles.prize_info}>
-                        <div className={styles.prize_name}>慕斯蛋糕商品名称</div>
-                        <div className={styles.prize_status}>待接单</div>
-                    </div>
-                    <div className={styles.prize_moment}>
-                        <div className={styles.prize_date}>2020/06/03</div>
-                        <div className={styles.prize_time}>20:06:03</div>
-                    </div>
-                    <div className={styles.divider}></div>
-                    <div className={styles.footer_btn}>
-                        <div className={styles.order_status}>接单</div>
-                        <div className={styles.order_detail}>查看</div>
-                    </div>
-                </div>
-                <div className={styles.list_item}>
-                    <div className={styles.prize_info}>
-                        <div className={styles.prize_name}>慕斯蛋糕商品名称</div>
-                        <div className={styles.prize_status}>待接单</div>
-                    </div>
-                    <div className={styles.prize_moment}>
-                        <div className={styles.prize_date}>2020/06/03</div>
-                        <div className={styles.prize_time}>20:06:03</div>
-                    </div>
-                    <div className={styles.divider}></div>
-                    <div className={styles.footer_btn}>
-                        <div className={styles.order_status}>接单</div>
-                        <div className={styles.order_detail}>查看</div>
-                    </div>
-                </div>
-                <div className={styles.list_item}>
-                    <div className={styles.prize_info}>
-                        <div className={styles.prize_name}>慕斯蛋糕商品名称</div>
-                        <div className={styles.prize_status}>待接单</div>
-                    </div>
-                    <div className={styles.prize_moment}>
-                        <div className={styles.prize_date}>2020/06/03</div>
-                        <div className={styles.prize_time}>20:06:03</div>
-                    </div>
-                    <div className={styles.divider}></div>
-                    <div className={styles.footer_btn}>
-                        <div className={styles.order_status}>接单</div>
-                        <div className={styles.order_detail}>查看</div>
-                    </div>
-                </div>
-                <div className={styles.list_item}>
-                    <div className={styles.prize_info}>
-                        <div className={styles.prize_name}>慕斯蛋糕商品名称</div>
-                        <div className={styles.prize_status}>待接单</div>
-                    </div>
-                    <div className={styles.prize_moment}>
-                        <div className={styles.prize_date}>2020/06/03</div>
-                        <div className={styles.prize_time}>20:06:03</div>
-                    </div>
-                    <div className={styles.divider}></div>
-                    <div className={styles.footer_btn}>
-                        <div className={styles.order_status}>接单</div>
-                        <div className={styles.order_detail}>查看</div>
-                    </div>
-                </div>
-                <div className={styles.list_item}>
-                    <div className={styles.prize_info}>
-                        <div className={styles.prize_name}>慕斯蛋糕商品名称</div>
-                        <div className={styles.prize_status}>待接单</div>
-                    </div>
-                    <div className={styles.prize_moment}>
-                        <div className={styles.prize_date}>2020/06/03</div>
-                        <div className={styles.prize_time}>20:06:03</div>
-                    </div>
-                    <div className={styles.divider}></div>
-                    <div className={styles.footer_btn}>
-                        <div className={styles.order_status}>接单</div>
-                        <div className={styles.order_detail}>查看</div>
-                    </div>
-                </div>
-                <div className={styles.list_item}>
-                    <div className={styles.prize_info}>
-                        <div className={styles.prize_name}>慕斯蛋糕商品名称</div>
-                        <div className={styles.prize_status}>待接单</div>
-                    </div>
-                    <div className={styles.prize_moment}>
-                        <div className={styles.prize_date}>2020/06/03</div>
-                        <div className={styles.prize_time}>20:06:03</div>
-                    </div>
-                    <div className={styles.divider}></div>
-                    <div className={styles.footer_btn}>
-                        <div className={styles.order_status}>接单</div>
-                        <div className={styles.order_detail}>查看</div>
-                    </div>
-                </div>
+                {
+                    listData.map(item => (
+                        <div className={styles.list_item} key={item.id}>
+                            <div className={styles.prize_info}>
+                                <div className={styles.prize_name}>{item.delivery_name}</div>
+                                <div className={styles.prize_status}>
+                                    {/* 0待接单 1配送中 2配送成功 3配送失败 */}
+                                    {
+                                        item.delivery_status == 0 ? "待接单" :
+                                            item.delivery_status == 1 ? "配送中" :
+                                                item.delivery_status == 2 ? "配送成功" :
+                                                    item.delivery_status == 3 ? "配送失败" : ""
+                                    }
+                                </div>
+                            </div>
+                            <div className={styles.prize_moment}>
+                                <div className={styles.prize_date}>{item.created_at}</div>
+                            </div>
+                            <div className={styles.divider}></div>
+                            <div className={styles.footer_btn}>
+                                <div className={styles.order_status}>
+                                    {/* 0待接单 1配送中 2配送成功 3配送失败 */}
+                                    {
+                                        item.delivery_status == 0 ? "接单" :
+                                            item.delivery_status == 1 ? "核销二维码" :
+                                                item.delivery_status == 2 ? "已完成" :
+                                                    item.delivery_status == 3 ? "配送失败" : ""
+                                    }
+                                </div>
+                                <div className={styles.order_detail} onClick={this.handleDetail.bind(this,item.id)}>查看</div>
+                            </div>
+                        </div>
+                    ))
+                }
+
             </div>
         )
     }
--- src/pages/activitys/dispatching/detail/index.tsx
+++ src/pages/activitys/dispatching/detail/index.tsx
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import { Modal } from 'antd-mobile';
 import styles from './index.less';
+import Request from '@/services/request';
+import router from 'umi/router';
 
+
+const alert = Modal.alert;
 class Detail extends Component {
+
+    state = {
+        info: {}
+    }
+
+    handleCancel = () => {
+        alert('提示', '确认取消该订单吗？', [
+            { text: '不取消', onPress: () => console.log('cancel'), style: 'default' },
+            { text: '取消', onPress: () => console.log('ok') },
+        ])
+    };
+
+    componentWillMount = () => {
+        const id = this.props.history.location.query.id;
+        Request({
+            url: `v3/merchant/delivery/order/${id}`,
+            method: 'GET'
+        }).then(res => {
+            if (res.code == 200) {
+                this.setState({
+                    info: res.data
+                })
+            }
+        })
+    }
+
     render() {
+        const { info } = this.state;
         return (
             <div className={styles.order_detail}>
                 <div className={styles.order_name}>
@@ -11,23 +43,71 @@ class Detail extends Component {
                 </div>
                 <div className={styles.flex_order_info}>
                     <div className={styles.order_info_status}>商品名称</div>
-                    <div className={styles.order_info_value}>商品名称是一个慕斯蛋糕</div>
+                    <div className={styles.order_info_value}>{info.delivery_name}</div>
                 </div>
                 <div className={styles.flex_order_info}>
                     <div className={styles.order_info_status}>配送状态</div>
-                    <div className={styles.order_info_value}>待接单</div>
+                    <div className={styles.order_status}>
+                        {/* 0待接单 1配送中 2配送成功 3配送失败 */}
+                        {
+                            info.delivery_status == 0 ? "待接单" :
+                                info.delivery_status == 1 ? "配送中" :
+                                    info.delivery_status == 2 ? "配送成功" :
+                                        info.delivery_status == 3 ? "配送失败" : ""
+                        }
+                    </div>
                 </div>
                 <div className={styles.flex_order_info}>
                     <div className={styles.order_info_status}>订单号</div>
-                    <div className={styles.order_info_value}>pc9527</div>
+                    <div className={styles.order_info_value}>{info.delivery_no}</div>
                 </div>
                 <div className={styles.flex_order_info}>
                     <div className={styles.order_info_status}>订单金额</div>
-                    <div className={styles.order_info_value}>99.99元</div>
+                    <div className={styles.order_info_value}>{info.delivery_money}元</div>
                 </div>
                 <div className={styles.flex_order_info}>
                     <div className={styles.order_info_status}>订单时间</div>
-                    <div className={styles.order_info_value}>2020-02-20 19:45:30</div>
+                    <div className={styles.order_info_value}>{info.delivery_time}</div>
+                </div>
+
+                <div className={styles.divider}></div>
+
+                <div className={styles.order_name}>
+                    <div className={styles.logo}></div>
+                    <div className={styles.title}>配送信息</div>
+                </div>
+                <div className={styles.flex_order_info}>
+                    <div className={styles.order_info_status}>配送人姓名</div>
+                    <div className={styles.order_info_value}>{info.user_name}</div>
+                </div>
+                <div className={styles.flex_order_info}>
+                    <div className={styles.order_info_status}>配送人电话</div>
+                    <div className={styles.order_info_value}>{info.user_mobile}</div>
+                </div>
+                <div className={styles.flex_order_info}>
+                    <div className={styles.order_info_status}>配送费用</div>
+                    <div className={styles.order_info_value}>{info.delivery_money}元</div>
+                </div>
+                <div className={styles.flex_order_info}>
+                    <div className={styles.order_info_status}>配送地址</div>
+                    <div className={styles.order_info_value}>{info.detail}</div>
+                </div>
+
+
+                {/* <div className={styles.footer_btn}>
+                    <div className={styles.cancel} onClick={this.handleCancel}>取消</div>
+                    <div className={styles.order_btn}>接单</div>
+                </div> */}
+                {/* <div className={styles.footer_btn}>
+                    <div className={styles.cancel} onClick={this.handleCancel}>取消</div>
+                    <div className={styles.order_btn}>配送</div>
+                </div> */}
+                {/* <div className={styles.footer_btn}>
+                    <div className={styles.cancel} onClick={this.handleCancel}>取消</div>
+                    <div className={styles.order_btn}>核销二维码</div>
+                </div> */}
+                <div className={styles.footer_btn}>
+                    <div className={styles.go_back} onClick={() => router.push('/activitys/dispatching/List')}>返回配送列表</div>
                 </div>
             </div>
         )
