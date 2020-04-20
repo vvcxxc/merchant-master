da5cdb4f63d1b32f8d0e812b19686935bd0b67c2
songyizhe <769724661@qq.com>
2020-04-20 11:44:04 +0800

    fix: 本地开发字体太小问题

--- .umirc.local.ts
+++ .umirc.local.ts
@@ -1,6 +1,28 @@
 import { IConfig } from "umi-types";
 
 const config: IConfig = {
+  chainWebpack: function (config, { webpack }) {
+    config.merge({
+      optimization: {
+        minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 30000,
+          minChunks: 3,
+          automaticNameDelimiter: '.',
+          cacheGroups: {
+            vendor: {
+              name: 'vendors',
+              test({ resource }) {
+                return /[\\/]node_modules[\\/]/.test(resource);
+              },
+              priority: 10,
+            },
+          },
+        },
+      }
+    });
+  },
   define: {
     "window.api": "http://test.api.supplier.tdianyi.com/",
     // "window.api": "http://release.api.supplier.tdianyi.com/",
