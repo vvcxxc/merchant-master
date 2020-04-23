9d4c24c5f2207ee9b818b88f59afae529e41f243
songyizhe <769724661@qq.com>
2020-04-23 17:25:36 +0800

    fix: 核销记录名称过长

--- src/pages/verification/item.tsx
+++ src/pages/verification/item.tsx
@@ -19,14 +19,14 @@ export default class VerificationItem extends Component<Item> {
 	}
 	render() {
 		// console.log(this.props,'8989898');
-		
+
 		return (
 			<Flex className={styles.item} onClick={this.my_click}>
 				<img src={this.props.image} className="img" alt="" />
 				<Flex.Item>
-					<Flex className="title">
-						{this.props.name}
-						<Flex.Item className="time">{this.props.confirm_time}</Flex.Item>
+					<Flex className="title" justify='between'>
+          <div className='name'>	{this.props.name}</div>
+						<div className="time">{this.props.confirm_time}</div>
 					</Flex>
 					<div className="info">核销会员：{this.props.user_name}</div>
 					<div className="info">订单号：{this.props.youhui_sn}</div>
